6af478740cf358222fd6411638a0ff5740ed15ec
Lucas ROdrigues <llucasy@hotmail.com>
2021-08-12 20:58:57 -0300

    The web framework

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+const app = express();
+import { createServer } from 'http';
+const server = createServer(app);
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
