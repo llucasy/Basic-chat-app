4d049e36aa1f6b162bfb6c7b7f807af945f471c0
llucasy <llucasy@hotmail.com>
2021-09-06 15:16:10 +0000

    Serving HTML

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 import express from 'express';
 const app = express();
 import { createServer } from 'http';
 const server = createServer(app);
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/index.html');
 });
 
 server.listen(3000, () => {
