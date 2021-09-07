aa1bb657496e8514cce3de735bf088f1dfe4c8ae
llucasy <llucasy@hotmail.com>
2021-09-07 18:51:45 +0000

    Integrating Socket.IO

--- index.js
+++ index.js
@@ -8,8 +8,18 @@ const app = express();
 import { createServer } from 'http';
 const server = createServer(app);
 
+import {Server} from 'socket.io'
+const io = new Server(server)
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 });
 
 server.listen(3000, () => {
